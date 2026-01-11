05f692676cdbb67aea91eb4042999b5cd3c7395d
seutje <seutje@gmail.com>
2026-01-11 15:09:21 +0100

    More particles!

--- constants.ts
+++ constants.ts
@@ -2,8 +2,8 @@ import { Project } from './types';
 import * as THREE from 'three';
 
 export const ATTRACTION_RADIUS = 10.0; // Increased radius for 3D volume
-export const PARTICLE_COUNT = 512; // Squared -> 512x512 = 262144 particles
-export const TEXTURE_SIZE = 512;
+export const PARTICLE_COUNT = 2048; // Squared -> 2048x2048 = 4194304 particles
+export const TEXTURE_SIZE = 2048;
 
 export const PROJECTS: Project[] = [
   {
